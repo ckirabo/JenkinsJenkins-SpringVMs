734f727887858f474bf40f3eeb6c6f9e594fbbc5
ckirabo <kirabo646@googlemail.com>
2019-01-10 10:51:48 +0000

    Update scriptForJenkins.bash

--- scriptForJenkins.bash
+++ scriptForJenkins.bash
@@ -1,14 +1,16 @@
 #Script for jenkins vm Jenkins
-ssh-keygen
+#sudo su
+#mkdir /var/lib/jenkins/.ssh
+#ssh-keygen
+#the place you want it saved /var/lib/jenkins/.ssh/id_rsa
 cd /var/lib/jenkins
 chown -R jenkins:jenkins .ssh
 chmod -R 600 .ssh
 ll
 cat .ssh/id_rsa.pub
-chmod 700 .s
 chmod 700 .ssh
 chmod 600 ~/.ssh/*
 ll
 #command below is ran after the spring vm is connected to jenkins. passing the keys over
-ssh -i .ssh/id_rsa jenkins@10.0.0.5
+#ssh -i .ssh/id_rsa jenkins@10.0.0.5
 
