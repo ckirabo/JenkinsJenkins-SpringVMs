bc5fbbf14cfbe5fa2c9cb22dc9459693745a454f
ckirabo <kirabo646@googlemail.com>
2019-01-10 11:59:59 +0000

    Update scriptForSpringVM.bash

--- scriptForSpringVM.bash
+++ scriptForSpringVM.bash
@@ -1,11 +1,16 @@
 #script for the Spring VM
+
+#$ cat ~/.ssh/id_rsa.pub
+#ssh clare@
+
 sudo user add jenkins
 sudo mkdir /home/jenkins
 sudo chown -R jenkins:jenkins /home/jenkins/
+sudo su -jenkins
 mkdir ~/.ssh
 vim ~/.ssh/authorized_keys
-chmod 700 .ssh/
-chmod 600 ~/.ssh/authorized_keys
+chmod 700 ~/.ssh
+chmod 600 ~/.ssh/*
 ll .ssh/
 ls -al .ssh/
 
